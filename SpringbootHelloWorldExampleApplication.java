df9c3be242e932eb934acc00367ba0350750675b
XJOEMANO <jmnaveenx@gmail.com>
2020-07-01 21:25:28 +0530

    Create SpringbootHelloWorldExampleApplication.java

---
+++ SpringbootHelloWorldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.hello.world.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloWorldExampleApplication.class, args);
+	}
+
+}
